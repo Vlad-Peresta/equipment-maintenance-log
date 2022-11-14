a7e46d44a5f56e28f0fcf38b30ac907dc64f174e
Vladyslav Peresta <perestavlad@gmail.com>
2022-11-14 23:54:51 +0200

    Register models for admin site

--- log/admin.py
+++ log/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from log.models import (
+    Position,
+    EquipmentType,
+    BreakdownType,
+    TaskType,
+    Equipment,
+    Breakdown,
+    Task,
+    Worker,
+)
+
+
+admin.site.register(Position)
+admin.site.register(EquipmentType)
+admin.site.register(BreakdownType)
+admin.site.register(TaskType)
+
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ("name", "inventory_number", "type")
+    search_fields = ("name",)
+    list_filter = ("type",)
+
+
+@admin.register(Breakdown)
+class BreakdownAdmin(admin.ModelAdmin):
+    list_display = (
+        "equipment", "breakdown_type", "time", "repair_duration", "status",)
+    search_fields = ("equipment", "breakdown_type",)
+    list_filter = ("equipment", "repair_staff", "breakdown_type", "time",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_completed", "task_type",)
+    search_fields = ("name",)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional info", {"fields": ("position",)}
+        ),
+
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": ("position",)
+            }
+        ),
+    )
