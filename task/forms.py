2a754a5f72ac5e26fa94d41d44015dc64dc50d5d
Vladyslav Peresta <perestavlad@gmail.com>
2022-12-14 23:48:06 +0200

    Added search functionality for pages `task-type-list` and `task-list`

--- task/forms.py
+++ task/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
-from task.models import TaskType
+from task.models import TaskType, Task
+from worker.models import Worker
 
 
 class TaskTypeForm(forms.ModelForm):
@@ -16,3 +17,109 @@ class TaskTypeForm(forms.ModelForm):
     class Meta:
         model = TaskType
         fields = ("name",)
+
+
+class TaskForm(forms.ModelForm):
+    task_type = forms.ModelChoiceField(
+        queryset=TaskType.objects.all(),
+        empty_label="Task type",
+        widget=forms.Select(
+            attrs={
+                "class": "form-select"
+            }
+        )
+    )
+    name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Task name",
+                "class": "form-control"
+            }
+        )
+    )
+    priority = forms.ChoiceField(
+        choices=Task.PRIORITY_CHOICES,
+        widget=forms.Select(
+            attrs={
+                "class": "form-select",
+            }
+        )
+    )
+    deadline = forms.DateField(
+        widget=forms.DateInput(
+            attrs={
+                "class": "form-control",
+                "type": "date",
+                "placeholder": "Deadline"
+            }
+        )
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=Worker.objects.all(),
+        widget=forms.SelectMultiple(
+            attrs={
+                "class": "form-select",
+            }
+        )
+    )
+
+    class Meta:
+        model = Task
+        fields = (
+            "task_type",
+            "name",
+            "priority",
+            "deadline",
+            "description",
+            "assignees"
+        )
+
+
+class TaskSearchForm(forms.Form):
+    searched_task_type = forms.CharField(
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by task type",
+                "class": "form-control"
+            }
+        )
+    )
+    searched_name = forms.CharField(
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by task name",
+                "class": "form-control"
+            }
+        )
+    )
+    searched_complete_status = forms.BooleanField(
+        required=False,
+        label="Tasks in process",
+        widget=forms.CheckboxInput(
+            attrs={
+                "class": "form-check-input",
+                "onclick": "this.form.submit();",
+            }
+        )
+    )
+
+
+class TaskTypeSearchForm(forms.Form):
+    searched_task_type = forms.CharField(
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by task type name",
+                "class": "form-control"
+            }
+        )
+    )
--- task/views.py
+++ task/views.py
@@ -2,19 +2,106 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 from django.urls import reverse_lazy
 
-from task.forms import TaskTypeForm
+from task.forms import TaskTypeForm, TaskForm, TaskSearchForm, TaskTypeSearchForm
 from task.models import Task, TaskType
 
 
 class TaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
+    queryset = Task.objects.all()
     template_name = "task/task_list.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskListView, self).get_context_data(**kwargs)
+
+        task_type = self.request.GET.get("searched_task_type", "")
+        name = self.request.GET.get("searched_name", "")
+        is_completed = self.request.GET.get("searched_complete_status", "")
+
+        context["search_form"] = TaskSearchForm(
+            initial={
+                "searched_task_type": task_type,
+                "searched_name": name,
+                "searched_complete_status": is_completed
+            }
+        )
+
+        return context
+
+    def get_queryset(self):
+        form = TaskSearchForm(self.request.GET)
+        queryset = self.queryset
+
+        if form.is_valid():
+            is_completed = form.cleaned_data["searched_complete_status"]
+            queryset = queryset.filter(
+                name__icontains=form.cleaned_data["searched_name"],
+                task_type__name__icontains=form.cleaned_data["searched_task_type"],
+            )
+
+            if is_completed:
+                queryset = queryset.filter(is_completed=False)
+
+        return queryset
+
+
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Task
+    fields = (
+            "task_type",
+            "name",
+            "priority",
+            "deadline",
+            "description",
+            "assignees"
+    )
+    template_name = "task/task_detail.html"
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "task/task_form.html"
+    success_url = reverse_lazy("task:task-list")
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "task/task_form.html"
+    success_url = reverse_lazy("task:task-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    template_name = "task/task_form.html"
+    success_url = reverse_lazy("task:task-list")
 
 
 class TaskTypeListView(LoginRequiredMixin, generic.ListView):
     model = TaskType
+    queryset = TaskType.objects.all()
     template_name = "task/task_type_list.html"
     context_object_name = "task_type_list"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskTypeListView, self).get_context_data(**kwargs)
+
+        task_type_name = self.request.GET.get("searched_task_type", "")
+
+        context["search_form"] = TaskTypeSearchForm(
+            initial={"searched_task_type": task_type_name}
+        )
+
+        return context
+
+    def get_queryset(self):
+        form = TaskTypeSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return self.queryset.filter(
+                name__icontains=form.cleaned_data["searched_task_type"]
+            )
 
 
 class TaskTypeCreateView(LoginRequiredMixin, generic.CreateView):
